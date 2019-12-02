0c83529e134ca139f249024c94b3692d7ab10028
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-12-02 20:23:54 +0200

    Add files via upload

---
+++ OpOverloading1/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0212_opov
+{
+    public class Point
+    {
+        public double X { get; protected set; }
+        public double Y { get; protected set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Point operator + (Point p1, Point p2)
+        {
+            Point result = new Point(p1.X + p2.X, p1.Y + p2.Y);
+            return result;
+        }
+
+        public static Point operator -(Point p1, Point p2)
+        {
+            Point result = new Point(p1.X - p2.X, p1.Y - p2.Y);
+            return result;
+        }
+
+        public static double operator *(Point p1, Point p2)
+        {
+            double result = p1.X *  p2.X + p1.Y * p2.Y;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"Point: ({X}, {Y})";
+        }
+    }
+}
---
+++ OpOverloading1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0212_opov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3, 5);
+            Point p2 = new Point(2.5, 7);
+            Point p3 = p1 + p2;
+            Point p4 = p1 - p2;
+            double mul = p1 * p2;
+            Console.WriteLine(p3);
+        }
+    }
+}
